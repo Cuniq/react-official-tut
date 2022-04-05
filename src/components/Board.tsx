eecdcbf2201e55709205efff0244bbb8c51be73b
Argyroudis Athanasios <argyroudisth@gmail.com>
2022-04-05 20:28:49 +0300

    Used loop to render squares

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -14,24 +14,18 @@ export default class Board extends React.Component<BoardProps> {
   }
 
   render() {
-    return (
-      <div>
+    const squares = [];
+
+    for (let i = 0; i <= 2; i++) {
+      squares.push(
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(3 * i + 0)}
+          {this.renderSquare(3 * i + 1)}
+          {this.renderSquare(3 * i + 2)}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      );
+    }
+
+    return <div>{squares}</div>;
   }
 }
