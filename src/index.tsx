54eb09a68a041316943306db64c06e1343645a85
Argyroudis Athanasios <argyroudisth@gmail.com>
2022-04-05 19:49:38 +0300

    Updated to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './components/Game';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Game from "./components/Game";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const rootContainer = ReactDOM.createRoot(document.getElementById("root")!);
+rootContainer.render(
   <React.StrictMode>
     <Game />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
